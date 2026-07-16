ed1d10d7fb60d79bc09eaad0cc29f1e1bb59cd04
Talhah Zahid <talhazahid415@gmail.com>
2026-07-16 16:56:04 +0500

    feat:Connect Neon Database

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import {sequelize} from './src/service/database.js';
+dotenv.config ();
 const app = express ();
-const port = 8000;
+const port = process.env.PORT || 3000;
 
 app.get ('/', (req, res) => {
   res.send ('Hello Server');
 });
 
-app.listen (port, () => {
-  console.log (`server is running at port ${port}`);
-});
+const startServer = async () => {
+  try {
+    await sequelize.authenticate ();
+    console.log ('✔ Database connected successfully!');
+    app.listen (port, () => {
+      console.log (`server is running at port ${port}`);
+    });
+  } catch (error) {
+    console.log ('😱 Unable to connect to the database', error);
+  }
+};
+
+startServer ();
---
+++ src/service/database.js
@@ -0,0 +1,14 @@
+import {Sequelize} from 'sequelize';
+
+import dotenv from 'dotenv';
+dotenv.config ();
+
+export const sequelize = new Sequelize (process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
